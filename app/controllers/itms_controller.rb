8338ec715844e42e48e9c212f8d90cabfeded83b
taka0126ito <taka.major@icloud.com>
2021-08-14 13:41:06 +0900

    データベースの削除、修正とCSSの導入

--- app/controllers/itms_controller.rb
+++
@@ -1,2 +0,0 @@
-class ItmsController < ApplicationController
-end
--- test/controllers/residences_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ResidencesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
