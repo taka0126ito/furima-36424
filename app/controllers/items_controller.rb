d22ba20369ca9718a036951ebc691c9ce622c776
taka0126ito <taka.major@icloud.com>
2021-08-18 20:00:59 +0900

    商品情報編集機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
   before_action :set_item, only: [:edit, :update, :show]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -17,13 +18,13 @@ def new
 end
 
 def edit
-  redirect_to root_path unless current_user.id == @item.user_id
+  
   
 end
 
  def update
  
- redirect_to root_path unless current_user.id == @item.user_id
+
  if @item.update(item_params)
   redirect_to item_path 
  else
@@ -51,6 +52,10 @@ end
  def set_item
   @item = Item.find(params[:id])
 end
-
+def move_to_index
+  unless @item.user_id == current_user.id
+    redirect_to action: :index
+  end
+end
 end
 
