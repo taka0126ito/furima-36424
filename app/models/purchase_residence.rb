bfa306774275635cfa659b57c6b8b61ceded523b
taka0126ito <taka.major@icloud.com>
2021-08-23 16:09:26 +0900

    依頼内容の修正

--- app/models/purchase_residence.rb
+++ app/models/purchase_residence.rb
@@ -1,6 +1,6 @@
 class PurchaseResidence
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :municipality, :address, :building_name, :phone_number, :shipping_area_id,
+  attr_accessor :user_id, :item_id, :postal_code, :municipality, :address, :building_name, :phone_number, :shipping_charge_id, :shipping_area_id,
                 :token
 
   with_options presence: true do
@@ -11,12 +11,13 @@ class PurchaseResidence
     validates :municipality
     validates :address
     validates :shipping_area_id, numericality: { other_than: 1 }
+    validates :shipping_charge_id, numericality: { other_than: 1 }
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
   end
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
     Residence.create(postal_code: postal_code, municipality: municipality, address: address, building_name: building_name,
-                     phone_number: phone_number, shipping_area_id: shipping_area_id, purchase_id: purchase.id)
+                     phone_number: phone_number, shipping_area_id: shipping_area_id, shipping_charge_id: shipping_charge_id, purchase_id: purchase.id)
   end
 end
